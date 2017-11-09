02b64c9559a73600c2a1a699b4aded41974fd9ea
Hovhannes Grigoryan <hgrigoryan@questrade.com>
2017-11-09 19:58:21 +0400

    Add demo date source

---
+++ data/db/db.js
@@ -0,0 +1,112 @@
+const events = [{
+	id: 1,
+    name: 'Go Native 2017',
+    description: 'Tech conferance in Yerevan',
+    location: 'Yerevan, Armenia',
+}];
+
+const talks = [{
+    id: 1,
+    speakerId: 1,
+    name: 'Xamarin vs native apps',
+    description: 'Xamarin'
+},
+{
+    id: 2,
+    speakerId: 3,
+    name: 'Intro to GraphQL',
+    description: 'GraphQL',
+},
+{
+    id: 3,
+    speakerId: 8,
+    name: 'Specifics of game development',
+    description: 'GameDev'
+},
+{
+    id: 4,
+    speakerId: 2,
+    name: 'Native UI automation for iOS',
+    description: 'iOS'
+},
+{
+    id: 5,
+    speakerId: 7,
+    name: 'Performance measurement tools for Android',
+    description: 'Android'
+},
+{
+    id: 6,
+    speakerId: 4,
+    name: 'An introduction to elastic stack',
+    description: 'elastic'
+},
+{
+    id: 7,
+    speakerId: 6,
+    name: 'C++ 17 is here',
+    description: 'C++'
+},
+{
+    id: 8,
+    speakerId: 9,
+    name: 'Shadowmatic',
+    description: 'Best game :)'
+}
+,
+{
+    id: 9,
+    speakerId: 5,
+    name: 'Node.JS V8 internals',
+    description: 'Node.JS'
+}];
+
+const speakers = [{
+    id: 1,
+    name: 'Samvel Siradeghyan',
+    twitter: '@samvel',
+},
+{
+	id: 2,
+    name: 'Davit Siradeghyan',
+    twitter: '@davit',
+},
+{
+	id: 3,
+    name: 'Hovhannes Grigoryan',
+    twitter: '@hovgrig',
+},
+{
+    id: 4,
+    name: 'Armen Sanasaryan',
+    twitter: '@armen',
+},
+{
+	id: 5,
+    name: 'Vardan Grigoryan',
+    twitter: '@vardan',
+},
+{
+    id: 6,
+    name: 'Gevorg Voskanyan',
+    twitter: '@gevorg',
+},
+{
+	id: 7,
+    name: 'Heghine Hakobyan',
+    twitter: '@heghine',
+},
+{
+    id: 8,
+    name: 'Narek Aghekyan',
+    twitter: '@narek',
+},
+{
+	id: 9,
+    name: 'Bagrat Dabaghyan',
+    twitter: '@bagrat',
+}];
+
+exports.events = events;
+exports.talks = talks;
+exports.speakers = speakers;
